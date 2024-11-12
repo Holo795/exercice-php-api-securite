caada2955d8d5e2e5b8d978b438416c0547dc6a5
Paulin <paulinsegura79@gmail.com>
2024-11-12 22:15:10 +0100

    Add: Create Listener to do ADMIN, user's who has create a Company

--- src/EventListener/CompanyCreationListener.php
+++ src/EventListener/CompanyCreationListener.php
@@ -12,6 +12,9 @@ use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Events;
 use Symfony\Bundle\SecurityBundle\Security;
 
+// Listen for the postPersist event on the Company entity
+// When a Company entity is persisted, this listener will create a new UserCompanyRole entity
+// and assign it to the currently authenticated user with the role ADMIN
 #[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: Company::class)]
 class CompanyCreationListener
 {
@@ -24,19 +27,24 @@ class CompanyCreationListener
         $this->entityManager = $entityManager;
     }
 
+    // Called after a Company entity is persisted
     public function postPersist(Company $company, PostPersistEventArgs $event): void
     {
+        // Get the currently authenticated user
         $user = $this->security->getUser();
 
+        // Exit if there is no authenticated user
         if (!$user) {
             return;
         }
 
+        // Create a new UserCompanyRole, assign it to the user, company, and set the role to ADMIN
         $userCompanyRole = new UserCompanyRole();
         $userCompanyRole->setCompany($company);
         $userCompanyRole->setUser($user);
         $userCompanyRole->setRole(Role::ADMIN);
 
+        // Persist and save the new UserCompanyRole entity
         $this->entityManager->persist($userCompanyRole);
         $this->entityManager->flush();
     }
